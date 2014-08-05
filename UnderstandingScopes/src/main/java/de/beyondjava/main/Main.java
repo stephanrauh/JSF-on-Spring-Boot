0305fcbc55f320539468b552099e102a33e4433a
stephanrauh <stephan.rauh@gmx.net>
2014-08-05 23:54:33 +0200

    added Neil Griffin's Spring view scope

--- UnderstandingScopes/src/main/java/de/beyondjava/main/Main.java
+++ UnderstandingScopes/src/main/java/de/beyondjava/main/Main.java
@@ -1,7 +1,26 @@
+/**
+ *  (C) 2013-2014 Stephan Rauh http://www.beyondjava.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.beyondjava.main;
+
+import java.util.HashMap;
 
 import javax.faces.webapp.FacesServlet;
 
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -25,6 +44,15 @@ public class Main extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Main.class);
     }
+    
+    @SuppressWarnings("serial")
+	@Bean public org.springframework.beans.factory.config.CustomScopeConfigurer customScopeConfigurer(){
+    	CustomScopeConfigurer csc = new CustomScopeConfigurer();
+    	csc.setScopes(new HashMap<String, Object>() {{
+    		put("view", new com.liferay.faces.demos.spring.ViewScope());
+    	}});
+    	return csc;
+    }
 
     @Bean
     public ServletRegistrationBean servletRegistrationBean() {
@@ -35,6 +63,6 @@ public class Main extends SpringBootServletInitializer {
     
     @Bean
     public EmbeddedServletContainerCustomizer embeddedServletCustomizer() {
-      return new MyEmbeddedServletContainerCustomizer();
+      return new Port80ServletContainerCustomizer();
     }
 }
