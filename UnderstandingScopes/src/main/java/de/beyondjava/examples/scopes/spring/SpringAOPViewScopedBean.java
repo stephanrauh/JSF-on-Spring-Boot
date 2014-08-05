d0ae7925e5daa76361f74691bf3d6ee32a35f994
stephanrauh <stephan.rauh@gmx.net>
2014-08-05 23:57:16 +0200

    added Neil Griffin's Spring view scope

---
+++ UnderstandingScopes/src/main/java/de/beyondjava/examples/scopes/spring/SpringAOPViewScopedBean.java
@@ -0,0 +1,39 @@
+/**
+ *  (C) 2013-2014 Stephan Rauh http://www.beyondjava.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.beyondjava.examples.scopes.spring;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(proxyMode=ScopedProxyMode.TARGET_CLASS, value="view")
+public class SpringAOPViewScopedBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	public int counter=0;
+	
+	public String getCounter() {
+		return String.valueOf(++counter);
+	}
+	
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+}
+
---
+++ UnderstandingScopes/src/main/java/de/beyondjava/examples/scopes/spring/SpringViewScopedBean.java
@@ -0,0 +1,38 @@
+/**
+ *  (C) 2013-2014 Stephan Rauh http://www.beyondjava.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.beyondjava.examples.scopes.spring;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("view")
+public class SpringViewScopedBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int counter=1;
+	
+	public int getCounter() {
+		return counter++;
+	}
+	
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+}
+
